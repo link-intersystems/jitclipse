8fc9b12461486df63ba9eecb2ccefd1e1bb4e140
René Link <rene.link@link-intersystems.com>
2021-02-21 18:12:17 +0100

    Added hotspot parsing test.

--- bundles/io.jitclipse/io.jitclipse.core.tests/src/io/jitclipse/core/resources/internal/HotspotLogFileTest.java
+++ bundles/io.jitclipse/io.jitclipse.core.tests/src/io/jitclipse/core/resources/internal/HotspotLogFileTest.java
@@ -1,17 +1,27 @@
 package io.jitclipse.core.resources.internal;
 
 import static org.junit.Assert.assertNotNull;
+import static org.junit.jupiter.api.Assertions.assertEquals;
 import static org.junit.jupiter.api.Assertions.assertFalse;
 import static org.junit.jupiter.api.Assertions.assertTrue;
 
+import java.util.List;
 import java.util.Optional;
 import java.util.concurrent.CountDownLatch;
 import java.util.concurrent.TimeUnit;
 
+import org.junit.jupiter.api.MethodOrderer.OrderAnnotation;
 import org.junit.jupiter.api.Test;
+import org.junit.jupiter.api.TestMethodOrder;
 
+import io.jitclipse.core.model.IClass;
+import io.jitclipse.core.model.IHotspotLog;
+import io.jitclipse.core.model.allocation.IEliminatedAllocationList;
+import io.jitclipse.core.model.lock.IOptimisedLockList;
+import io.jitclipse.core.model.suggestion.ISuggestionList;
 import io.jitclipse.core.resources.IHotspotLogFile;
 
+@TestMethodOrder(OrderAnnotation.class)
 class HotspotLogFileTest extends AbstractJitProjectTest {
 
 	@Test
@@ -23,6 +33,15 @@ class HotspotLogFileTest extends AbstractJitProjectTest {
 
 	@Test
 	void openHotspotLogFile() throws InterruptedException {
+		IHotspotLogFile hotspotLogFile = doOpenHotspotLogFile();
+		assertNotNull(hotspotLogFile);
+		assertTrue(hotspotLogFile.isOpened());
+
+		IHotspotLog hotspotLog = hotspotLogFile.getHotspotLog();
+		assertNotNull(hotspotLog);
+	}
+
+	private IHotspotLogFile doOpenHotspotLogFile() throws InterruptedException {
 		Optional<IHotspotLogFile> hotspotLogFileOptional = hotspotLogFolder.getHotspotLogFile("hotspot.log");
 		assertTrue(hotspotLogFileOptional.isPresent());
 
@@ -49,8 +68,29 @@ class HotspotLogFileTest extends AbstractJitProjectTest {
 		hotspotLogFileOptional.ifPresent(hlf -> hlf.open(hotspotLogHolder::setHotspotLogFile, null));
 
 		IHotspotLogFile hotspotLogFile = hotspotLogHolder.getHotspotLogFile();
-		assertNotNull(hotspotLogFile);
-		assertTrue(hotspotLogFile.isOpened());
+		return hotspotLogFile;
+	}
+
+	@Test
+	void getHotspotLog() throws InterruptedException {
+		IHotspotLogFile hotspotLogFile = doOpenHotspotLogFile();
+		IHotspotLog hotspotLog = hotspotLogFile.getHotspotLog();
+		assertNotNull(hotspotLog);
+
+		List<IClass> classes = hotspotLog.getClasses();
+		assertEquals(75, classes.size());
+
+		ISuggestionList suggestionList = hotspotLog.getSuggestionList();
+		assertNotNull(suggestionList);
+		assertEquals(21, suggestionList.size());
+
+		IOptimisedLockList optimizedLockList = hotspotLog.getOptimizedLockList();
+		assertNotNull(optimizedLockList);
+		assertEquals(0, optimizedLockList.size());
+
+		IEliminatedAllocationList eliminatedAllocationList = hotspotLog.getEliminatedAllocationList();
+		assertNotNull(eliminatedAllocationList);
+		assertEquals(2, eliminatedAllocationList.size());
 	}
 
 }
